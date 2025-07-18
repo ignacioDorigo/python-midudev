786f2f66674b5ba2cdd49fc767f783e8c2c104b2
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-07-17 23:57:14 -0300

    primer commit

---
+++ 01_basic/01_print.py
@@ -0,0 +1,13 @@
+nombre = "Nacho"
+apellido = "Dorigo"
+
+# Print con espacios
+print(nombre,apellido)
+
+# Print con guiones
+print(nombre,apellido,sep="-")
+
+# Print en una linea
+print(nombre,end=" ")
+print(apellido)
+
---
+++ 01_basic/02_types.py
@@ -0,0 +1,19 @@
+print()
+# Tipos de Datos en Python
+
+# Integers
+numeroEntero = 33
+print(numeroEntero)
+print(type(numeroEntero))
+
+
+# Float (decimal)
+numeroFlotante = 3.1415
+print(numeroFlotante)
+print(type(numeroFlotante))
+
+
+# Booleans (bool)
+print(True)
+print(False)
+print(type(True))
